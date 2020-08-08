acba057bedd455cdb902c7aecdbc863e028a97fa
Irieda Noboru <irieda@144lab.com>
2020-08-08 20:39:00 +0900

    err obj wrapped

--- util.go
+++ util.go
@@ -135,7 +135,7 @@ func CallbackN(fn func(res []js.Value) interface{}) js.Func {
 type wrappedError js.Value
 
 func (w wrappedError) Error() string {
-	return w.String()
+	return js.Value(w).String()
 }
 
 func (w wrappedError) JSValue() js.Value {
