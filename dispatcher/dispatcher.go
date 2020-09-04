b95aadacbbc8fccad9c8413ecca6de730310d219
Irieda Noboru <irieda@144lab.com>
2020-09-04 10:46:59 +0900

    dispatcher remove unregister function

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -9,19 +9,6 @@ func Register(a Action, callback func()) {
 	registered[a] = append(registered[a], callback)
 }
 
-// Unregister ...
-func Unregister(a Action, callback func()) {
-	list := registered[a]
-	replace := []func(){}
-	for i, v := range list {
-		if v==callback [
-			continue
-		]
-		replace = append(replace, v)
-	}
-	registered[a] = replace
-}
-
 // Dispatch ...
 func Dispatch(a Action) {
 	for _, v := range registered[a] {
